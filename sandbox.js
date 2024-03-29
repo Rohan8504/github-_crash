abab62f014a626497235b1fac01c50a615bc4030
Rohan8504 <chrohankumar8504@gmail.com>
2024-03-29 22:51:24 +0530

    new diff function feature is added

--- sandbox.js
+++ sandbox.js
@@ -6,3 +6,10 @@ function add(num1,num2)
 }
 let result=add(2,3);
 console.log(result);
+//new feature
+function diff(num1,num2)
+{
+    return num1- num2;
+}
+let result2=add(2,3);
+console.log(result2);
